9c55df583fce64b85c33aa9fa692ff6d61db65cf
vytre iud <vytre932@outlook.com>
2018-01-09 13:15:35 +0100

    canvis

--- Project2/GraphD.cpp
+++ Project2/GraphD.cpp
@@ -6,6 +6,19 @@ GraphD::GraphD()
 {
 }
 
+GraphD::GraphD(std::vector<std::pair<int, int>> myArcs)
+{
+
+	for (int i = 0; i <= myArcs.size(); i++)
+	{
+		
+		tmpNode.info = myArcs[i].first;
+		nodeMap[myArcs[i].first] = 
+	}
+}
+
+
+
 
 GraphD::~GraphD()
 {
--- Project2/GraphD.h
+++ Project2/GraphD.h
@@ -1,9 +1,19 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <list>
+#include <map>
+struct node{
+	std::list<int> adyacencias;
+	int info;
+};
 class GraphD
 {
 	
+	std::map<int,std::list<int>> nodeMap;
 public:
 	GraphD();
+	GraphD(std::vector<std::pair<int,int>> myArcs);
 	~GraphD();
 };
-
